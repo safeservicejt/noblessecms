600939d4fc4499a29900ebaa4636811b20133e6a
James <safeservicejt@gmail.com>
2015-04-05 07:33:03 +0700

    Improve classes

--- contents/themes/inax/view/home.php
+++
@@ -1,5 +0,0 @@
-                    <?php echo $content_top;?>
-                        <?php echo $content_left;?>
-                        <?php echo $content_right;?>
-                        <?php echo $content_bottom;?>
-
