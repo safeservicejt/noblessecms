ebe0bf3f17d010ea0bad898994f36fee42f36f4f
safeservicejt <safeservicejt@gmail.com>
2015-09-29 20:09:11 +0700

    Fix ckeditor encode html output

--- bootstrap/ckeditor/config.js
+++ bootstrap/ckeditor/config.js
@@ -30,6 +30,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
+	config.htmlEncodeOutput = false;
+	
+	config.entities = false;
+
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
