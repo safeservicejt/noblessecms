34ad34aa3715e0d797e6b33b2efb88cf4c8450f2
safeservicejt <safeservicejt@gmail.com>
2015-11-10 01:10:37 +0700

    Improve system

--- application/views/admincp/themeList.php
+++ application/views/admincp/themeList.php
@@ -33,8 +33,8 @@
 				<!-- row -->
 				<div class="row">
 					<div class="col-lg-12">
-					<a href="<?php echo System::getUrl();?>admincp/theme/setting/<?php echo THEME_NAME;?>" class="btn btn-primary">Setting</a>
-					<a href="<?php echo System::getUrl();?>admincp/theme/edit/<?php echo THEME_NAME;?>" class="btn btn-danger">Edit</a>
+					<a href="<?php echo System::getUrl();?>admincp/theme/setting/<?php echo System::getThemeName();?>" class="btn btn-primary">Setting</a>
+					<a href="<?php echo System::getUrl();?>admincp/theme/edit/<?php echo System::getThemeName();?>" class="btn btn-danger">Edit</a>
 
 					</div>					
 				</div>
