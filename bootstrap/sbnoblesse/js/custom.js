f04b4751c85a70a3deb23964fadbde59446e271e
safeservicejt <safeservicejt@gmail.com>
2015-11-26 13:57:29 +0700

    Improve system

--- bootstrap/sbnoblesse/js/custom.js
+++ bootstrap/sbnoblesse/js/custom.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
 
 	$('.img-tools').animo( { animation: ['tada', 'bounce'], duration: 2 } );
 
+	$('.img-tools').mouseover(function() {
+		$(this).css({
+			'opacity': '0.7'
+		}).animo( { animation: ['tada', 'bounce'], duration: 2 } );
+	});
+	$('.img-tools').mouseout(function() {
+		$(this).css({
+			'opacity': '1'
+		})
+	});
+
 });
