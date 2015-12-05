e616c66966734f9c04ee20de7d98b8ac744f9523
safeservicejt <safeservicejt@gmail.com>
2015-12-05 11:59:03 +0700

    Register system disabled on default

--- includes/System.php
+++ includes/System.php
@@ -573,7 +573,7 @@ class System
 	public static function makeSetting()
 	{
 		$settingData=array(
-			'system_status'=>'working','system_mode'=>'basic', 'system_lang'=>'en', 'register_user_status'=>'enable',
+			'system_status'=>'working','system_mode'=>'basic', 'system_lang'=>'en', 'register_user_status'=>'disable',
 			'default_member_groupid'=>'2', 'default_member_banned_groupid'=>'5', 'default_dateformat'=>'M d, Y',
 			'rss_status'=>'enable','comment_status'=>'enable', 'title'=>'Noblesse CMS Website', 'keywords'=>'noblessecms, blog, website',
 			'descriptions'=>'Noblesse CMS Website Description','default_page_method'=>'none','default_page_url'=>'','default_timezone'=>'US/Arizona',
