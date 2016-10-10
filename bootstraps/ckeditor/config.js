64cb0ee7a97eee0a527ad058985e1568d554c849
safeservicejt <safeservicejt@gmail.com>
2016-10-10 13:31:50 +0700

    Improve system

--- bootstraps/ckeditor/config.js
+++ bootstraps/ckeditor/config.js
@@ -3,41 +3,6 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-config.wordcount = {
-
-    // Whether or not you want to show the Paragraphs Count
-    showParagraphs: true,
-
-    // Whether or not you want to show the Word Count
-    showWordCount: true,
-
-    // Whether or not you want to show the Char Count
-    showCharCount: false,
-
-    // Whether or not you want to count Spaces as Chars
-    countSpacesAsChars: false,
-
-    // Whether or not to include Html chars in the Char Count
-    countHTML: false,
-    
-    // Maximum allowed Word Count, -1 is default for unlimited
-    maxWordCount: -1,
-
-    // Maximum allowed Char Count, -1 is default for unlimited
-    maxCharCount: -1,
-
-    // Add filter to add or remove element before counting (see CKEDITOR.htmlParser.filter), Default value : null (no filter)
-    filter: new CKEDITOR.htmlParser.filter({
-        elements: {
-            div: function( element ) {
-                if(element.attributes.class == 'mediaembed') {
-                    return false;
-                }
-            }
-        }
-    })
-};
-
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
@@ -80,4 +45,40 @@ CKEDITOR.editorConfig = function( config ) {
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
+
+	config.wordcount = {
+
+	    // Whether or not you want to show the Paragraphs Count
+	    showParagraphs: true,
+
+	    // Whether or not you want to show the Word Count
+	    showWordCount: true,
+
+	    // Whether or not you want to show the Char Count
+	    showCharCount: false,
+
+	    // Whether or not you want to count Spaces as Chars
+	    countSpacesAsChars: false,
+
+	    // Whether or not to include Html chars in the Char Count
+	    countHTML: false,
+	    
+	    // Maximum allowed Word Count, -1 is default for unlimited
+	    maxWordCount: -1,
+
+	    // Maximum allowed Char Count, -1 is default for unlimited
+	    maxCharCount: -1,
+
+	    // Add filter to add or remove element before counting (see CKEDITOR.htmlParser.filter), Default value : null (no filter)
+	    filter: new CKEDITOR.htmlParser.filter({
+	        elements: {
+	            div: function( element ) {
+	                if(element.attributes.class == 'mediaembed') {
+	                    return false;
+	                }
+	            }
+	        }
+	    })
+	};
+	
 };
